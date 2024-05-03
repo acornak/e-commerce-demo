08e57dc19f4c2c0a7aeb45f84c5d036def1b4715
“anton-cornak” <anton.cornak@visma.com>
2024-05-04 01:27:37 +0200

    oopsie

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 // Next
 import { NextPage } from "next";
 // Components
@@ -6,13 +6,20 @@ import StyledHero from "@/components/hero/StyledHero";
 import NewsletterBanner from "@/components/common/Newsletter";
 import ProductGrid from "@/components/product/ProductGrid";
 import ProductsFilter from "@/components/product/ProductsFilter";
+import StyledLoading from "@/components/styled/Loading";
 // Images
 import productsHero from "@/public/products/products_hero.webp";
 
 // https://pertic-store-demo.myshopify.com/collections/all
 const ProductsPage: NextPage = (): JSX.Element => {
 	return (
-		<>
+		<Suspense
+			fallback={
+				<div className="flex items-center justify-center h-screen">
+					<StyledLoading />
+				</div>
+			}
+		>
 			<StyledHero image={productsHero} link="products" title="Products" />
 			<div className="flex flex-col lg:flex-row">
 				<div className="lg:w-1/4 w-full">
@@ -24,7 +31,7 @@ const ProductsPage: NextPage = (): JSX.Element => {
 			</div>
 
 			<NewsletterBanner />
-		</>
+		</Suspense>
 	);
 };
 
